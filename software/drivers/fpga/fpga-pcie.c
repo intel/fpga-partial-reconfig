6c58bc1496c08d90947ea83bfe4778a8f9096d48
Kalen Brunham <kalen.brunham@intel.com>
2017-05-09 13:58:32 -0400

    Updated license header in design files.

--- software/drivers/fpga/fpga-pcie.c
+++ software/drivers/fpga/fpga-pcie.c
@@ -1,3 +1,20 @@
+/*
+ *
+ * Copyright (C) 2017 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "altera-pr-ip-core.h"
 #include <linux/debugfs.h>
 #include <linux/delay.h>
