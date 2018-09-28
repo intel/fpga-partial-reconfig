8e54147d5ecb3e9b7bfa06e9ea6b9229d5ee4019
Yuan Cai <yuan.cai@intel.com>
2018-09-28 15:14:25 -0700

    Update S10 HPR PCIe Ref Design to 18.1

--- ref_designs/s10_pcie_devkit_hpr/compile.sh
+++ ref_designs/s10_pcie_devkit_hpr/compile.sh
@@ -3,9 +3,8 @@
 set -e
 
 quartus_sh --flow compile s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr
-quartus_cdb -r s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr --export_block root_partition --snapshot final --file s10_pcie_devkit_hpr_static.qdb --preserve_sdc
 quartus_sh --flow compile s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr_ddr4_access
-quartus_cdb -r s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr_ddr4_access --export_block pr_partition --snapshot final --file s10_pcie_devkit_hpr_ddr4_access_pr_partition_final.qdb --preserve_sdc
+quartus_cdb -r s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr_ddr4_access --export_block pr_partition --snapshot final --file output_files/s10_pcie_devkit_hpr_ddr4_access_pr_partition_final.qdb --preserve_sdc
 quartus_sh --flow compile s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr_basic_arithmetic
 quartus_sh --flow compile s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr_basic_dsp
 quartus_sh --flow compile s10_pcie_devkit_hpr -c s10_pcie_devkit_hpr_gol
