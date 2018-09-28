073801422193375a06aae0c3c9cfa55e338650b5
Yuan Cai <yuan.cai@intel.com>
2018-09-28 14:08:56 -0700

    Update S10 PR over PCIE ref design to 18.1

--- ref_designs/s10_pcie_devkit_pr/compile.sh
+++ ref_designs/s10_pcie_devkit_pr/compile.sh
@@ -3,7 +3,7 @@
 set -e
 
 quartus_sh --flow compile s10_pcie_devkit_pr -c s10_pcie_devkit_pr
-quartus_cdb -r s10_pcie_devkit_pr -c s10_pcie_devkit_pr --export_block root_partition --snapshot final --file s10_pcie_devkit_pr_static.qdb --preserve_sdc
+#quartus_cdb -r s10_pcie_devkit_pr -c s10_pcie_devkit_pr --export_block root_partition --snapshot final --file s10_pcie_devkit_pr_static.qdb --preserve_sdc
 quartus_sh --flow compile s10_pcie_devkit_pr -c s10_pcie_devkit_pr_basic_arithmetic
 quartus_sh --flow compile s10_pcie_devkit_pr -c s10_pcie_devkit_pr_basic_dsp
 quartus_sh --flow compile s10_pcie_devkit_pr -c s10_pcie_devkit_pr_gol
