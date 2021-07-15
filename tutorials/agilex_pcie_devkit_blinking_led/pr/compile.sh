cdbdcb39363782a3f74fec1aa75be79913733a1d
nssyafawani <83336905+nssyafawani@users.noreply.github.com>
2021-07-15 10:50:17 +0800

    Delete tutorials/agilex_pcie_devkit_blinking_led directory

--- tutorials/agilex_pcie_devkit_blinking_led/pr/compile.sh
+++
@@ -1,5 +0,0 @@
-set -e
-quartus_sh --flow compile blinking_led -c blinking_led
-quartus_sh --flow compile blinking_led -c blinking_led_slow
-quartus_sh --flow compile blinking_led -c blinking_led_empty
-quartus_sh --flow compile blinking_led -c blinking_led_default
